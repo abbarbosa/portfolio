8b4e1a8351a1c6083c3e0c4ee323063ff99dc3bc
Annabia <annabbarbosa2712@gmail.com>
2024-11-25 21:31:37 -0300

    subindo curriculo

--- src/Pages/AboutMe/index.jsx
+++ src/Pages/AboutMe/index.jsx
@@ -62,10 +62,10 @@ function AboutMe() {
             </div>
 
             <div className="w-full flex items-center justify-center gap-4 px-4">
-                <a href="https://drive.google.com/file/d/1mz4R9ZD5gAX4F_QgANDkw7fyWCfJwXb7/view?usp=sharing" target="_blank">
+                {/* <a href="https://drive.google.com/file/d/1mz4R9ZD5gAX4F_QgANDkw7fyWCfJwXb7/view?usp=sharing" target="_blank">
                     <button className="text-white text-[24px] p-2 rounded-[50px] hover:underline decoration-[#7877DF] hover:decoration-4 underline-offset-4 decoration-rounded">vizualizar portfólio</button>
-                </a>
-                <a href="https://drive.google.com/file/d/1HOHViwP66Kw44-ba-7D5VaxUgXkKvCjV/view?usp=sharing" target="_blank" >
+                </a> */}
+                <a href="https://drive.google.com/file/d/1OdDVB1wy6N_MRmsmnJWyIwCdDN2iYvZR/view?usp=sharing" target="_blank" >
                     <button className="text-white text-[24px] p-2 rounded-[50px] hover:underline decoration-[#7877DF] hover:decoration-4 underline-offset-4 decoration-rounded">vizualizar curriculo</button>
                 </a>
             </div>
